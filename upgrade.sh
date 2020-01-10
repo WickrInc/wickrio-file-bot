f29f6a63e075ad43c8c7e94839b5b424932bc70b
Paul Cushman <pcushman@wickr.com>
2020-01-10 10:14:40 -0800

    fix the upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -11,6 +11,6 @@ version=`cat "$old_version"`
 cd $OLD_FILEBOT_LOCATION
 cp -r files $NEW_FILEBOT_LOCATION
 cd ..
-mv file_bot file_bot.old_V$version
+mv wickrio-file-bot wickrio-file-bot.old_V$version
 cd $NEW_FILEBOT_LOCATION/..
 mv $NEW_FILEBOT_LOCATION file_bot
