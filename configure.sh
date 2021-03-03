fe610363b98b825eda56d7b70aafb0586b0f8ddb
pwcushman <pcushman@wickr.com>
2021-03-03 09:28:50 -0500

    use node version 12 (#17)

--- configure.sh
+++ configure.sh
@@ -10,6 +10,11 @@ if [ -n "$1" ]; then
   fi
 fi
 
+if [ -f "/usr/local/nvm/nvm.sh" ]; then
+  . /usr/local/nvm/nvm.sh
+  nvm use 12.20.2
+fi
+
 if [ -z "$CLIENT_NAME" ]; then
   echo "prompt: Please enter your client bot's username:"
   while [ -z "$input" ]
