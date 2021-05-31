7893c73bf1a445819d9e03b7a5d6c1eb8f209d90
toren <tderosa@wickr.com>
2021-05-31 16:01:33 -0400

    WIP: Using wickrio-bot-api for configure

--- configure.sh
+++ configure.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 #
+# Remove the configure environment variable file
+#
+rm -f .env.configure
+
+#
 # If the input argument exists then check if it is a file
 # if so it should contain a list of key=value entries.
 #
 if [ -n "$1" ]; then
   if [ -f "$1" ]; then
     . "$1"
+    cp "$1" .env.configure
   fi
 fi
 
@@ -15,21 +21,9 @@ if [ -f "/usr/local/nvm/nvm.sh" ]; then
   nvm use 12.20.2
 fi
 
-if [ -z "$CLIENT_NAME" ]; then
-  echo "prompt: Please enter your client bot's username:"
-  while [ -z "$input" ]
-  do
-    read  input
-    if [ ! -z "$input" ]
-    then
-      echo ${input} >client_bot_username.txt
-      WICKRIO_BOT_NAME=${input} node configure.js
-    else
-      echo "Cannot leave client bot's username empty! Please enter a value:"
-    fi
-  done
+if [ -z "$CLIENT_NAME" ];
+then
+  node configure.js
 else
-  echo $CLIENT_NAME >client_bot_username.txt
   WICKRIO_BOT_NAME=$CLIENT_NAME node configure.js
 fi
-
